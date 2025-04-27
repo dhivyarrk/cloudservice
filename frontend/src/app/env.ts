13073c6aead553fee747c79a241618eb4aa7876d
dhivyarrk <dhivyabharathi.radhakrishnanr@gmail.com>
2025-04-27 19:32:29 +0300

    Add backend url for angular env [build-frontend]

--- frontend/src/app/env.ts
+++ frontend/src/app/env.ts
@@ -1,2 +1,8 @@
 //export const API_URL = 'http://localhost:5000'; 
-export const API_URL = 'https://booboofashionsgunic.onrender.com';
+//export const API_URL = 'http://127.0.0.1:5000';
+//export const API_URL = 'http://127.0.0.1:5000/api';
+//export const API_URL = 'http://localhost/api'; 
+//export const API_URL = 'http://backend:5000/api';
+////export const API_URL = 'http://172.17.0.2:5000';
+//export const API_URL = 'https://booboofashionsgunic.onrender.com';
+export const API_URL = 'https://booboofashions.com/api';
